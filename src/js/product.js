97c01dc71d12567264a56f5167aa10d03dbf5bf9
Sehee Jang <seheejang94@gmail.com>
2025-01-11 19:18:42 +0900

    fix: addProductToCart

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,19 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const cart = getLocalStorage("so-cart") || [];
+
+  if (!Array.isArray(cart)) {
+    console.error("Invalid cart format. Resetting cart.");
+    setLocalStorage("so-cart", []);
+    return;
+  }
+
+  cart.push(product);
+  setLocalStorage("so-cart", cart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
