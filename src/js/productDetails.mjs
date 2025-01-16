a50cd65b032d22343fa9c07c97d96bc6d705337b
Sariah <sariahwandler@gmail.com>
2025-01-16 13:12:54 -0700

    finished individual activity

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -1,23 +1,23 @@
 import {getLocalStorage, setLocalStorage} from './utils.mjs';
 
  //create template for html
- function productDetailsTemplate(product) {
-    return `<section class="product-detail"> <h3>${product.Brand.Name}</h3>
-      <h2 class="divider">${product.NameWithoutBrand}</h2>
-      <img
-        class="divider"
-        src="${product.Image}"
-        alt="${product.NameWithoutBrand}"
-      />
-      <p class="product-card__price">$${product.FinalPrice}</p>
-      <p class="product__color">${product.Colors[0].ColorName}</p>
-      <p class="product__description">
-      ${product.DescriptionHtmlSimple}
-      </p>
-      <div class="product-detail__add">
-        <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
-      </div></section>`;
-  };
+function productDetailsTemplate(product) {
+return `<section class="product-detail"> <h3>${product.Brand.Name}</h3>
+    <h2 class="divider">${product.NameWithoutBrand}</h2>
+    <img
+    class="divider"
+    src="${product.Image}"
+    alt="${product.NameWithoutBrand}"
+    />
+    <p class="product-card__price">$${product.FinalPrice}</p>
+    <p class="product__color">${product.Colors[0].ColorName}</p>
+    <p class="product__description">
+    ${product.DescriptionHtmlSimple}
+    </p>
+    <div class="product-detail__add">
+    <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
+    </div></section>`;
+};
 
 export default class ProductDetails {
     //constructor
