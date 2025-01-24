687588ba2c5bdeedc9a5568c9bf1127fba0ee476
Sehee Jang <seheejang94@gmail.com>
2025-01-24 23:54:24 +0900

    add: W03 Individual Activity: Expand Inventory - Step 3

--- vite.config.js
+++ vite.config.js
@@ -8,10 +8,11 @@ export default defineConfig({
     outDir: "../dist",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'src/index.html'),
-        cart: resolve(__dirname, 'src/cart/index.html'),
-        checkout: resolve(__dirname, 'src/checkout/index.html'),
-        product: resolve(__dirname, 'src/product_pages/index.html'),
+        main: resolve(__dirname, "src/index.html"),
+        category: resolve(__dirname, "src/product-listing/index.html"),
+        cart: resolve(__dirname, "src/cart/index.html"),
+        checkout: resolve(__dirname, "src/checkout/index.html"),
+        product: resolve(__dirname, "src/product_pages/index.html"),
       },
     },
   },
